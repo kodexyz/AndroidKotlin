1ae431e612b41ce46cf1156f495f4d19f8b77e6f
AwesomeHaircut <jesusbalbastro@gmail.com>
2017-08-08 04:52:57 -0700

    Got a button to work!

--- app/src/main/java/coursera/week2/MainActivity.kt
+++ app/src/main/java/coursera/week2/MainActivity.kt
@@ -2,11 +2,22 @@ package coursera.week2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val next = findViewById<View>(R.id.buttonKilotoMiles) as Button
+        next.setOnClickListener{
+            view -> Snackbar.make(
+                view, "Tomaaato", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
+
